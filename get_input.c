1224f2c259da4fd75b52538b653e27f1cdc9a160
Muhammadd08 <sobhymuhammad8@gmail.com>
2023-10-10 10:12:19 -0700

    S

--- get_input.c
+++ get_input.c
@@ -5,12 +5,9 @@ int get_input()
 	char *buff;
 	size_t n;
 	int bytes;
-	printf("in function");
+
 	buff = (char *)malloc(sizeof(char));
-	printf("malloc done");
 	n = 1;
-	printf("size n done");
-	bytes = getline(&buff, &n, 0);
-	printf("getline done");
+	bytes = getline(&buff, &n, STDIN_FILENO);
 	return (bytes);
 }
--- shell.c
+++ shell.c
@@ -2,11 +2,14 @@
 
 int main()
 {
+	int w;
 	char *buff = "#Simple_Shell$ ";
+
 	while (1)
 	{
-		write(STDOUT_FILENO, buff, 15);
-		printf("write done");
+		w = write(STDOUT_FILENO, buff, 15);
+		if (w == -1)
+			return (1);
 		get_input();
 	}
 	return (0);
