829b00c06949872c099acc7918f63c34161311d0
IslamAhmed <eslamelbeltagye1111@gmail.com>
2023-10-13 09:18:11 +0300

    modify the get input

--- exec.c
+++ exec.c
@@ -1,11 +1,21 @@
 #include "shell.h"
 
-void exec(__attribute__((unused)) char **buff)
+/**
+ * exec - function that execute the command
+ *
+ * @argv: The array of string (commands)
+*/
+
+void exec(__attribute__((unused)) char **argv)
 {
 	int r;
+	const char *buffer = argv[0];
 
-	const char *buffer = buff[0];
-	r = execve(buffer, buff, NULL);
-	perror("./shell");
-	exit(1);
+	system(buffer);
+	r = execve(argv[0], argv, NULL);
+	if (r == -1)
+	{
+		perror("./hsh");
+		exit(1);
+	}
 }
--- get_input.c
+++ get_input.c
@@ -1,15 +1,31 @@
 #include "shell.h"
 
-char *get_input(char *buffer)
-{
-	char *buff = NULL;
-	size_t n = 0, x;
+/**
+ * get_input - function to get input and put it into array
+ *
+ * Return: Array of argument
+*/
 
+char *get_input()
+{
+	char *buff;
+	size_t n = BUFF_SIZE, x;
+
+	buff = malloc(sizeof(char) * n);
+	if (buff == NULL)
+	{
+		perror("Error\n");
+		exit(1);
+	}
+	/*Get the input*/
 	x = getline(&buff, &n, stdin);
+	/*To Delete The newline*/
+	buff[strlen(buff) - 1] = '\0';
+	if (buff[0] == EOF)
+		printf("Exit");
 	if (x <= 0)
 		return (NULL);
-	buffer = strtok(buff, "\n");
-	if (buffer == NULL)
-		return (NULL);
-	return (buffer);
+
+	/*return the input*/
+	return (buff);
 }
